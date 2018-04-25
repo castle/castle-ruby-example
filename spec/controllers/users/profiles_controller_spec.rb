633349bab7c9457d0be739e68d75764abc05afd4
Maciej Mensfeld <maciej@mensfeld.pl>
2018-04-25 20:52:54 +0200

    message spy fix

--- spec/controllers/users/profiles_controller_spec.rb
+++ spec/controllers/users/profiles_controller_spec.rb
@@ -13,14 +13,16 @@ RSpec.describe Users::ProfilesController do
     context 'when authenticated user wants to edit his profile' do
       with_user
 
+      before { allow(controller.castle).to receive(:track) }
+
       it 'renders the edit template' do
         expect(response).to have_http_status(:ok)
         expect(edit_page).to render_template(:edit)
       end
 
       it 'does not trigger castle tracking for page view' do
-        expect(controller.castle).not_to receive(:track)
         edit_page
+        expect(controller.castle).not_to have_received(:track)
       end
     end
   end
