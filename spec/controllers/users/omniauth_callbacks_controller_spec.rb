9fa9fd003fa384f500320611b08c0f4eedb82f3d
Maciej Mensfeld <maciej@mensfeld.pl>
2018-04-25 21:54:25 +0200

    omniauth full spec

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe Users::OmniauthCallbacksController do
     end
 
     context 'when user is valid and persisted' do
+      let(:castle_auth_args) do
+        {
+          event: '$login.succeeded',
+          user_id: user.id,
+          user_traits: user_attributes
+        }
+      end
+
       before do
         allow(controller.castle).to receive(:authenticate).and_return(verdict)
         get :twitter
@@ -30,11 +38,7 @@ RSpec.describe Users::OmniauthCallbacksController do
         it 'expect to run castle authentication' do
           expect(controller.castle)
             .to have_received(:authenticate)
-            .with(
-              event: '$login.succeeded',
-              user_id: user.id,
-              user_traits: user_attributes
-            )
+            .with(castle_auth_args)
         end
       end
 
@@ -48,18 +52,23 @@ RSpec.describe Users::OmniauthCallbacksController do
         it 'expect to run castle authentication' do
           expect(controller.castle)
             .to have_received(:authenticate)
-            .with(
-              event: '$login.succeeded',
-              user_id: user.id,
-              user_traits: user_attributes
-            )
+            .with(castle_auth_args)
         end
       end
 
       context 'when user denied' do
         let(:verdict) { { action: 'deny' } }
 
-        pending
+        it 'expect to redirect to sign in path with a proper message' do
+          expect(response).to redirect_to new_user_session_path
+          expect(flash['error']).to eq I18n.t('users.omniauth_callbacks.twitter.access_denied')
+        end
+
+        it 'expect to run castle authentication' do
+          expect(controller.castle)
+            .to have_received(:authenticate)
+            .with(castle_auth_args)
+        end
       end
     end
 
