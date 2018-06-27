e08f42f8a1d17b428d1577fe067a5f39f5626d67
Maciej Mensfeld <maciej@mensfeld.pl>
2018-06-27 14:41:47 +0200

    profile integration specs

--- spec/controllers/users/profiles_controller_spec.rb
+++ spec/controllers/users/profiles_controller_spec.rb
@@ -28,6 +28,64 @@ RSpec.describe Users::ProfilesController do
   end
 
   describe 'POST #update' do
-    pending
+    subject(:update_page) { put :update, params: params }
+
+    let(:params) { {} }
+
+    context 'when unauthenticated user wants to update his profile' do
+      it 'redirects to a sign in path' do
+        expect(update_page).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'when authenticated user wants to update his profile' do
+      with_user
+
+      context 'with invalid data' do
+        let(:params) { { user: { email: '' } } }
+        let(:track_expected_data) do
+          {
+            event: '$profile_update.failed',
+            user_id: controller.current_user.id,
+            user_traits: controller.current_user.attributes
+          }
+        end
+
+        before { allow(controller.castle).to receive(:track) }
+
+        it 'renders the edit template' do
+          expect(response).to have_http_status(:ok)
+          expect(update_page).to render_template(:edit)
+        end
+
+        it 'expect to trigger castle tracking for failed profile update' do
+          update_page
+          expect(controller.castle).to have_received(:track).with(track_expected_data)
+        end
+      end
+
+      context 'with valid data' do
+        let(:params) { { user: { email: Faker::Internet.email } } }
+        let(:track_expected_data) do
+          {
+            event: '$profile_update.succeeded',
+            user_id: controller.current_user.id,
+            user_traits: controller.current_user.attributes
+          }
+        end
+
+        before { allow(controller.castle).to receive(:track) }
+
+        it 'renders the edit template' do
+          expect(response).to have_http_status(:ok)
+          expect(update_page).to redirect_to root_path
+        end
+
+        it 'expect to trigger castle tracking for succeeded profile update' do
+          update_page
+          expect(controller.castle).to have_received(:track).with(track_expected_data)
+        end
+      end
+    end
   end
 end
