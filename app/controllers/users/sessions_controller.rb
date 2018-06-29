91fb10644f08197243b26578b2f843e195773a37
Maciej Mensfeld <maciej@mensfeld.pl>
2018-06-29 11:06:41 +0200

    better warden

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,25 +8,25 @@ module Users
     # Key that is used in Devise for user authentication
     AUTHENTICATION_KEY = 'email'
 
-    # Due to the fact, that Devise uses internally warden for authentication
-    # this is a trick not to have to inject middleware into warden and to have
-    # everything session - castle related in one place
-    after_action :track_failed_login, only: :new, if: :failed_login?
-
     # Sign in with Castle tracking.
     # @note For now we allow user when verdict is not deny. Challenge could be implemented
     def create
-      warden.authenticate!(auth_options)
-
-      case authenticate_with_castle(current_user)[:action]
-      when 'allow'
-        super
-      when 'challenge'
-        super
-      when 'deny'
-        warden.logout
-        flash[:error] = t('.access_denied')
-        redirect_to new_user_session_url
+      if warden.authenticate(auth_options)
+        case authenticate_with_castle(current_user)[:action]
+        when 'allow'
+          super
+        when 'challenge'
+          super
+        when 'deny'
+          warden.logout
+          flash[:error] = t('.access_denied')
+          redirect_to new_user_session_url
+        end
+      else
+        user_details = request.filtered_parameters.fetch('user') { {} }
+        user = User.find_by(AUTHENTICATION_KEY => user_details[AUTHENTICATION_KEY])
+        castle.track(event: '$login.failed', user_id: user&.id, user_traits: user_details)
+        throw(:warden)
       end
     end
 
@@ -36,25 +36,10 @@ module Users
       # tried to logout
       user_id = current_user&.id
       super
-
       castle.track(event: '$logout.succeeded', user_id: user_id)
     end
 
     private
-
-    # Tracks any failed login attemps with information (if available) for which user
-    # this attempt has failed
-    def track_failed_login
-      user_details = request.filtered_parameters.fetch('user') { {} }
-      user = User.find_by(AUTHENTICATION_KEY => user_details[AUTHENTICATION_KEY])
-
-      castle.track(event: '$login.failed', user_id: user&.id)
-    end
-
-    # @return [Boolean] true if this was a failed login attempt
-    def failed_login?
-      (options = request.env['warden.options']) && options[:action].to_s == 'unauthenticated'
-    end
 
     # Authenticates user in Castle
     # @param current_user [User]
